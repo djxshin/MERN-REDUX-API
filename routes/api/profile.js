8519df43d3629f783a866ab90f95d80cdea11cd6
DJ Shin <djatlienriseup@gmail.com>
2019-07-19 15:43:26 -0400

    profiles and profile id postman

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -110,7 +110,44 @@ router.post('/', [auth, [
     }
 );
 
+// @route           POST api/profile/me
+// @description     Create or update user profile 
+// @access          Private 
 
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles)
+    } catch (error) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+// @route           Get api/profile/user/:user_id
+// @description     Get profile by user ID
+// @access          Private 
+
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({
+            user: req.params.user_id
+        }).populate('user', ['name', 'avatar']);
+        if (!profile) return res.status(400).json({
+            message: "Profile not Found"
+        })
+
+        res.json(profile)
+    } catch (err) {
+        console.error(err.message)
+        if (err.kind == 'ObjectId') {
+            return res.status(400).json({
+                message: "Profile not found"
+            });
+        }
+        res.status(500).send('Server Error')
+    }
+})
 
 
 module.exports = router;
