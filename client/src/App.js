0ef3c9109641b8d4fdd32f9f166c71c9e4ebe417
DJ Shin <djatlienriseup@gmail.com>
2019-08-02 18:14:17 -0400

    auth reducer and register action

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,19 @@ import Navbar from './components/layouts/Navbar'
 import Landing from './components/layouts/Landing'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
+import Alert from './components/layouts/Alert.js'
+// redux imports
+import {Provider} from 'react-redux';
+import store from './store'
 
 const App = ()=>  
+<Provider store={store}>
   <Router>
     <Fragment>
       <Navbar/>
     <Route exact path="/" component={Landing}/>
     <section className="container">
+      <Alert/>
       <Switch>
           <Route exact path="/register" component={Register}/>
           <Route exact path="/login" component={Login}/>
@@ -19,5 +25,6 @@ const App = ()=>
     </section>
     </Fragment>
   </Router>
+  </Provider>
 
 export default App;
